6de6bb23bbcc95993c6aaffe7482ef605b4b6bb4
Stevo <sdebaun@sparks.network>
2016-04-20 08:42:04 -0700

    processes nonce and stores result

--- src/tasks/Engagements.js
+++ src/tasks/Engagements.js
@@ -1,13 +1,14 @@
 import {isAdmin, isUser} from './authorization'
 
 const create = (values, uid, {gateway, Profiles, Engagements, Projects}) =>
-  gateway.generateclientToken()
+  gateway.generateClientToken()
+  // .then(response => response.clientToken)
   // new Promise((resolve) =>
   //   gateway.clientToken.generate({}, (err,response) =>
   //     resolve(response.clientToken)
   //   )
   // )
-  .then(clientToken =>
+  .then(({clientToken}) =>
     Engagements.push({...values,
       isApplied: true,
       isAccepted: false,
@@ -28,10 +29,39 @@ const remove = (key, uid, {Profiles, Engagements, Projects}) =>
 const update = ({key, values}, uid, {Engagements}) =>
   Engagements.child(key).update(values).then(ref => key)
 
-const pay = ({key, values}, uid, {Engagements}) => {
+const extractAmount = s =>
+  parseInt(s.replace(/[^0-9\.]/g, ''), 10)
 
-  Engagements.child(key).update(values).then(ref => key)
-}
+const pay = ({key, values}, uid, {Engagements, Commitments, gateway}) =>
+  Engagements.get(key).then(({oppKey}) =>
+    Commitments.by('oppKey', oppKey)
+  )
+  .then(commits => ({
+    payment: commits.find(({code}) => code === 'payment'),
+    deposit: commits.find(({code}) => code === 'deposit'),
+  }))
+  .then(c => ({
+    payment: extractAmount(c.payment && c.payment.amount || 0),
+    deposit: extractAmount(c.deposit && c.deposit.amount || 0),
+  }))
+  // .then(amounts => console.log('payment amounts found', amounts))
+  .then(amounts =>
+    gateway.createTransaction({
+      amount: amounts.payment,
+      paymentMethodNonce: values.paymentNonce,
+    }, {
+      submitForSettlement: true,
+    })
+    .then(({transaction}) =>
+      Engagements.child(key).update({
+        transaction,
+        amountPaid: transaction.amount,
+        isPaid: true,
+      })
+    )
+    .then(() => key)
+    .catch(errorResult => console.log('BRAINTREE TRANSACTION ERROR', errorResult))
+  )
 
 export default {
   create,
