2f8326a7ec3654d8352bd1339218e6a70d144a24
Jeremy Wells <jemmyw@gmail.com>
2016-06-22 15:47:16 +1200

    fix(Arrivals,Seneca): Fix the expected arguments of the arrivals create action

--- src/tasks/Arrivals.js
+++ src/tasks/Arrivals.js
@@ -1,23 +1,29 @@
-import {join, always} from 'ramda'
+import {join, always, when} from 'ramda'
 
-const joinedKeys = ({projectKey, profileKey}) =>
+const joinedKeys = (projectKey, profileKey) =>
   join('-', [projectKey, profileKey])
 
 function actions({auths: {userCanUpdateProject}, models: {Arrivals}}) {
-  this.add({role:'Arrivals',cmd:'create'}, ({uid, values}, respond) =>
-    userCanUpdateProject({uid, projectKey: values.projectKey})
-    .then(data =>
-      Arrivals.first('projectKeyProfileKey', joinedKeys(values))
-      .catch(always(data))
-      .then(arrival => arrival ? Promise.reject('Already arrived') : data)
-      .then(() => Arrivals.push({
-        ...values,
-        arrivedAt: Date.now(),
-        projectKeyProfileKey: joinedKeys(values),
-        ownerProfileKey: data.profile.$key,
-      }).key()))
-    .then(key => respond(null, {key}))
-    .catch(err => respond(err)))
+  this.add(
+    {role:'Arrivals',cmd:'create'},
+    ({uid, profile, profileKey, projectKey}, respond) =>
+      Arrivals.first('projectKeyProfileKey', joinedKeys(projectKey, profileKey))
+        .catch(always(null))
+        .then(when(Boolean, () => Promise.reject('Already arrived')))
+        .then(() => Arrivals.push({
+          projectKey,
+          profileKey,
+          arrivedAt: Date.now(),
+          projectKeyProfileKey: joinedKeys(projectKey, profileKey),
+          ownerProfileKey: profile.$key,
+        }).key())
+        .then(key => respond(null, {key})))
+
+  this.wrap({role: 'Arrivals', cmd:'create'}, function(msg, respond) {
+    userCanUpdateProject(msg)
+      .then(data => this.prior({...msg, ...data}, respond))
+      .catch(err => respond(err))
+  })
 }
 
 export default actions
