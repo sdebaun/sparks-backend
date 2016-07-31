030104810df67750d77409f5dfc0092eeeffc040
Jeremy Wells <jemmyw@gmail.com>
2016-07-31 13:37:51 +1200

    chore(): Modify memberships create action to use values as standard

--- src/tasks/Memberships.js
+++ src/tasks/Memberships.js
@@ -1,7 +1,9 @@
 import defaults from './defaults'
 
 function Memberships() {
-  this.add({role:'Memberships',cmd:'create'}, async function({teamKey, oppKey, engagementKey, answer}) {
+  this.add({role:'Memberships',cmd:'create'}, async function({values}) {
+    const {teamKey, oppKey, engagementKey, answer} = values
+
     return await this.act('role:Firebase,model:Memberships,cmd:push', {values: {
       teamKey,
       oppKey,
